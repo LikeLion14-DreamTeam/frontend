f57212e8fa9d11e6f4338abcf5fc9edb46425573
h1gyeon9 <nhk1007@naver.com>
2026-08-10 14:27:34 +0900

    feat: 페이지 라우팅 설정

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,20 @@
+import { Routes, Route } from "react-router-dom"
+import Home from "./pages/Home"
+import Record from "./pages/Record"
+import Recommendation from "./pages/Recommendation"
+import Archive from "./pages/Archive"
+import Trip from "./pages/Trip"
+
+
 function App() {
   return (
-    <p>Hello World</p>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/record" element={<Record />} />
+      <Route path="/recommendation" element={<Recommendation />} />
+      <Route path="/archive" element={<Archive />} />
+      <Route path="/trip" element={<Trip />} />
+    </Routes>
   )
 }
 
