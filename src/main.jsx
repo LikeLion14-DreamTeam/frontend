a4b9d811983cbe91cdbf35ae50f5062f3fe319d5
jwlee <jwon04lee@gmail.com>
2026-08-12 20:31:36 +0900

    feat: 구글 맵 API 적용, 포토북 상세 화면에 테스트로 표시

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,14 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
+import { APIProvider } from '@vis.gl/react-google-maps'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <APIProvider apiKey={import.meta.env.VITE_GOOGLE_MAPS_API_KEY ?? ''}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </APIProvider>
   </StrictMode>,
 )
